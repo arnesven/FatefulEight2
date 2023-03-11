c032e9dae4df0c5fb1010df19ac69910d8df5c2e
erik <eanilsson88@gmail.com>
2023-03-11 18:50:25 +0100

    Some usage stats on skills

--- src/model/classes/Skill.java
+++ src/model/classes/Skill.java
@@ -1,31 +1,31 @@
 package model.classes;
 
 public enum Skill implements Comparable<Skill> {
-    Acrobatics("Acrobatics"),
-    Axes("Axes"),
-    Blades("Blades"),
-    BluntWeapons("Blunt Weapons"),
-    Bows("Bows"),
-    Endurance("Endurance"),
-    Entertain("Entertain"),
-    Labor("Labor"),
-    Leadership("Leadership"),
-    Logic("Logic"),
-    MagicBlack("Magic (Black)"),
-    MagicBlue("Magic (Blue)"),
-    MagicGreen("Magic (Green)"),
-    MagicRed("Magic (Red)"),
-    MagicWhite("Magic (White)"),
-    MagicAny("Magic (Any)"),
-    Perception("Perception"),
-    Persuade("Persuade"),
-    Polearms("Polearms"),
-    Search("Search"),
-    Security("Security"),
-    SeekInfo("Seek Info"),
-    Sneak("Sneak"),
-    SpellCasting("Spell Casting"),
-    Survival("Survival"),
+    Acrobatics("Acrobatics"),       // 20
+    Axes("Axes"),                   // 8
+    Blades("Blades"),               // 15
+    BluntWeapons("Blunt Weapons"),  // 14
+    Bows("Bows"),                   // 10
+    Endurance("Endurance"),         // 20
+    Entertain("Entertain"),         // 18
+    Labor("Labor"),                 // 16
+    Leadership("Leadership"),       // 18
+    Logic("Logic"),                 // 17
+    MagicBlack("Magic (Black)"),    // 9
+    MagicBlue("Magic (Blue)"),      // 8
+    MagicGreen("Magic (Green)"),    // 10
+    MagicRed("Magic (Red)"),        // 7
+    MagicWhite("Magic (White)"),    // 9
+    MagicAny("Magic (Any)"),        // 6
+    Perception("Perception"),       // 27
+    Persuade("Persuade"),           // 26
+    Polearms("Polearms"),           // 8
+    Search("Search"),               // 20
+    Security("Security"),           // 16
+    SeekInfo("Seek Info"),          // 20
+    Sneak("Sneak"),                 // 25
+    SpellCasting("Spell Casting"),  // 10
+    Survival("Survival"),           // 31
 
     UnarmedCombat("Unarmed Combat");
 
