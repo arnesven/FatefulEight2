18d7ac0c89f7834795c4117675d55bd438eccb3f
erik <eanilsson88@gmail.com>
2023-03-18 23:48:20 +0100

    Nicer select starting character menu

--- src/model/states/ChooseStartingCharacterState.java
+++ src/model/states/ChooseStartingCharacterState.java
@@ -3,9 +3,14 @@ package model.states;
 import model.Model;
 import model.characters.GameCharacter;
 import util.MyRandom;
+import view.ArrowMenuGameView;
+import view.GameView;
 import view.SelectStaringCharacterView;
 import view.help.TutorialStartDialog;
 import view.party.CharacterCreationView;
+import view.subviews.ArrowMenuSubView;
+
+import java.util.List;
 
 public class ChooseStartingCharacterState extends GameState {
 
@@ -16,14 +21,28 @@ public class ChooseStartingCharacterState extends GameState {
     @Override
     public GameState run(Model model) {
         print("How would you like to select your starting character? ");
+        model.getLog().waitForAnimationToFinish();
         GameCharacter gc;
         while (true) {
-            print("Select (S) character, Random (R) character, Custom (C) character: ");
-            char selection = lineInput().toUpperCase().charAt(0);
+            char[] choice = new char[1];
+            String choices = "SRC";
+            model.setSubView(new ArrowMenuSubView(model.getSubView(),
+                    List.of("Choose character",
+                            "Random character",
+                            "Create custom character"),
+                    26, 16, ArrowMenuSubView.NORTH_WEST) {
+                @Override
+                protected void enterPressed(Model model, int cursorPos) {
+                    choice[0] = choices.charAt(cursorPos);
+                    model.setSubView(getPrevious());
+                }
+            });
+            waitForReturnSilently();
+            char selection = choice[0];
             if (selection == 'C') {
                 CharacterCreationView charCreation = new CharacterCreationView(model.getView());
                 model.transitionToDialog(charCreation);
-                print("Custom Character. ");
+                print(" ");
                 model.getLog().waitForAnimationToFinish();
                 gc = charCreation.getFinishedCharacter();
                 if (gc != null) {
@@ -32,7 +51,7 @@ public class ChooseStartingCharacterState extends GameState {
             } else if (selection == 'S') {
                 SelectStaringCharacterView selectChar = new SelectStaringCharacterView(model);
                 model.transitionToDialog(selectChar);
-                print("Select Character. ");
+                print(" ");
                 model.getLog().waitForAnimationToFinish();
                 gc = selectChar.getFinalCharacter();
                 if (gc != null) {
@@ -45,6 +64,7 @@ public class ChooseStartingCharacterState extends GameState {
                 break;
             }
         }
+        println("");
         print("You have selected your starting character: ");
         model.getParty().add(gc);
         model.getParty().addToGold(gc.getCharClass().getStartingGold());
