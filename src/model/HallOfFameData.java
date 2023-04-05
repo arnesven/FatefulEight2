92054c0e8b2874813db55080c49e4241df29b25b
erik <eanilsson88@gmail.com>
2023-04-05 13:51:36 +0200

    Sorting fix

--- src/model/HallOfFameData.java
+++ src/model/HallOfFameData.java
@@ -3,13 +3,12 @@ package model;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 
 public class HallOfFameData extends ArrayList<HallOfFameEntry> implements Serializable {
 
     public void append(Party party, GameScore score) {
         HallOfFameEntry e = new HallOfFameEntry(party.getPartyMembers(), score.getTotal());
         add(e);
-        Collections.sort(this, Comparator.comparingInt(HallOfFameEntry::getScore));
+        Collections.sort(this, (e1, e2) -> e2.getScore() - e1.getScore());
     }
 }
