dbc5df2a33955a79597bbfb46b2cb528846a1d3a
erik <eanilsson88@gmail.com>
2023-06-06 18:35:39 +0200

    Fixed bandit archer crash

--- src/model/enemies/BanditArcherEnemy.java
+++ src/model/enemies/BanditArcherEnemy.java
@@ -7,8 +7,8 @@ import view.sprites.Sprite;
 
 public class BanditArcherEnemy extends BanditEnemy {
 
-    private static final LoopingSprite humanSprite = new BanditSprite(Race.NORTHERN_HUMAN.getColor(), 0x120);
-    private static final LoopingSprite halfOrcSprite = new BanditSprite(Race.HALF_ORC.getColor(), 0x120);
+    private static final LoopingSprite humanSprite = new BanditSprite(Race.NORTHERN_HUMAN.getColor(), 0xC0);
+    private static final LoopingSprite halfOrcSprite = new BanditSprite(Race.HALF_ORC.getColor(), 0xC0);
 
     public BanditArcherEnemy(char a) {
         super(a, "Bandit Archer", 4);
