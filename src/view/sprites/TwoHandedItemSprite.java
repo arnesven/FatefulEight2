656fa4d52e0d6479853fa73378205105e4d20f8d
erik <eanilsson88@gmail.com>
2023-08-07 17:57:44 +0200

    Fix two-handed sprite bug, missing roman numeral 2

--- src/view/sprites/TwoHandedItemSprite.java
+++ src/view/sprites/TwoHandedItemSprite.java
@@ -4,7 +4,7 @@ import view.MyColors;
 
 public class TwoHandedItemSprite extends OverlayItemSprite {
     private static final Sprite ROMAN_TWO = new Sprite32x32("romantwo", "items.png", 0x0E,
-            MyColors.BLACK, MyColors.BLACK, MyColors.BLACK);
+            MyColors.GRAY, MyColors.BLACK, MyColors.BLACK);
 
     public TwoHandedItemSprite(int col, int row, MyColors color2, MyColors color3, MyColors color4) {
         super(col, row, color2, color3, color4, ROMAN_TWO);
