c2e4c3be647f3024f8c59e6b35ea8ec0c04e6566
erik <eanilsson88@gmail.com>
2023-10-05 11:42:55 +0200

    Added distilling info in tutorial text

--- src/view/help/TutorialAlchemy.java
+++ src/view/help/TutorialAlchemy.java
@@ -5,7 +5,7 @@ import view.GameView;
 public class TutorialAlchemy extends HelpDialog {
     private static final String TEXT =
             "The alchemy spell lets the caster convert gathered ingredients into " +
-            "potions. Only potions which you already have in your inventory can be " +
+            "potions (brewing) or vice versa (distilling). Only potions which you already have in your inventory can be " +
             "brewed. The number of required ingredients is equal to half the potions " +
             "value.\n\n" +
             "Ingredients can be found in many locations in the world and can often be " +
