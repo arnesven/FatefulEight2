a92c81f11738cd8e06cbe6d6b90e60335bd9bf60
erik <eanilsson88@gmail.com>
2023-10-14 18:27:35 +0200

    Upgraded archer event to dark deeds event

--- src/model/states/events/ArcherEvent.java
+++ src/model/states/events/ArcherEvent.java
@@ -4,15 +4,24 @@ import model.Model;
 import model.characters.GameCharacter;
 import model.characters.appearance.CharacterAppearance;
 import model.classes.Classes;
+import model.enemies.Enemy;
+import model.items.Equipment;
+import model.items.accessories.EmeraldRing;
+import model.items.clothing.StuddedJerkin;
+import model.items.weapons.CompositeBow;
 import model.states.DailyEventState;
+import util.MyRandom;
 import view.subviews.PortraitSubView;
 
-public class ArcherEvent extends DailyEventState {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArcherEvent extends DarkDeedsEvent {
     private final boolean withIntro;
     private final CharacterAppearance appearance;
 
     public ArcherEvent(Model model, boolean withIntro, CharacterAppearance appearance) {
-        super(model);
+        super(model, "Talk to", MyRandom.randInt(10, 20));
         this.withIntro = withIntro;
         this.appearance = appearance;
     }
@@ -22,12 +31,19 @@ public class ArcherEvent extends DailyEventState {
     }
 
     @Override
-    protected void doEvent(Model model) {
-        showExplicitPortrait(model, appearance, "Archer");
+    protected boolean doIntroAndContinueWithEvent(Model model) {
         if (withIntro) {
             println("Out on the grounds, a skilled archer puts arrow after " +
-                    "arrow right in the bullseye. He gladly gives all who will " +
-                    "listen a free lesson in marksmanship.");
+                    "arrow right in the bullseye.");
+        }
+        showExplicitPortrait(model, appearance, "Archer");
+        return true;
+    }
+
+    @Override
+    protected boolean doMainEventAndShowDarkDeeds(Model model) {
+        if (withIntro) {
+            println("The archer gladly gives all who will listen a free lesson in marksmanship.");
             println("Each party member gains 10 experience.");
             for (GameCharacter gc : model.getParty().getPartyMembers()) {
                 model.getParty().giveXP(model, gc, 10);
@@ -36,5 +52,26 @@ public class ArcherEvent extends DailyEventState {
         print("The archer offers to train you in the ways of being a Marksman, ");
         ChangeClassEvent change = new ChangeClassEvent(model, Classes.MAR);
         change.areYouInterested(model);
+        setCurrentTerrainSubview(model);
+        showExplicitPortrait(model, appearance, "Archer");
+        return true;
+    }
+
+    @Override
+    protected GameCharacter getVictimCharacter(Model model) {
+        GameCharacter gc = new GameCharacter("Archer", "", appearance.getRace(), Classes.MAR,
+                appearance, Classes.NO_OTHER_CLASSES, new Equipment(new CompositeBow(), new StuddedJerkin(), new EmeraldRing()));
+        gc.setLevel(MyRandom.randInt(3, 6));
+        return gc;
+    }
+
+    @Override
+    protected List<Enemy> getVictimCompanions(Model model) {
+        return new ArrayList<>();
+    }
+
+    @Override
+    protected ProvokedStrategy getProvokedStrategy() {
+        return ProvokedStrategy.FIGHT_IF_ADVANTAGE;
     }
 }
