da00e171637dbf8c704cd632ee0a206a31c38b4c
erik <eanilsson88@gmail.com>
2024-01-27 22:21:06 +0100

    Round Par -> Combat Par

--- src/view/help/CombatStatisticsChapter.java
+++ src/view/help/CombatStatisticsChapter.java
@@ -12,7 +12,7 @@ public class CombatStatisticsChapter extends SubChapterHelpDialog {
             "because of reduction from Armor Points.\n\n" +
             "Avoided Damage - The amount of damage which party members or allies did not take " +
             "because of blocking or evading.\n\n" +
-            "Round Par - An estimated number of round in which the combat should have been able to be fought, based on " +
+            "Combat Par - An estimated number of rounds in which the combat should have been able to be fought, based on " +
                     "the average damage of party members (and allies) and the total sum of the enemies' health points.\n\n" +
             "MVP - Most Valued Party Member\n" +
             "The character who killed the most enemies during combat.";
