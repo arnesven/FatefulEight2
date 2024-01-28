b7e2939dacb7ed28c03e490143973800dd43127c
erik <eanilsson88@gmail.com>
2024-01-28 21:27:12 +0100

    Some statistics in personality traits

--- src/model/characters/PersonalityTrait.java
+++ src/model/characters/PersonalityTrait.java
@@ -1,30 +1,31 @@
 package model.characters;
 
 public enum PersonalityTrait {
-    aggressive,
-    anxious,
-    benevolent,
-    calm,
-    cold,
-    cowardly,
-    critical,
-    diplomatic,
-    encouraging,
-    forgiving,
-    friendly,
-    gluttonous,
-    irritable,
-    intellectual,
-    jovial,
-    generous,
-    greedy,
-    mischievous,
-    narcissistic,
-    playful,
-    prude,
-    romantic,
-    rude,
-    snobby,
-    stingy,
-    unkind,
+                 // Chars    Usages
+    aggressive,  // 4        8
+    anxious,     // 3        6
+    benevolent,  // 4        10
+    calm,        // 8        4
+    cold,        // 4        5
+    cowardly,    // 4        8
+    critical,    // 6        8
+    diplomatic,  // 6        4
+    encouraging, // 4        5
+    forgiving,   // 4        3
+    friendly,    // 8        4
+    gluttonous,  // 4        5
+    irritable,   // 5        5
+    intellectual,// 6        4
+    jovial,      // 4        5
+    generous,    // 4        7
+    greedy,      // 5        5
+    mischievous, // 7        2
+    narcissistic,// 5        5
+    playful,     // 5        7
+    prude,       // 4        3
+    romantic,    // 6        6
+    rude,        // 4        7
+    snobby,      // 4        5
+    stingy,      // 4        6
+    unkind,      // 4        8
 }
