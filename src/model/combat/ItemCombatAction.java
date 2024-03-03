48b8546762d02f6c39c91c2da127e3d9ca037248
erik <eanilsson88@gmail.com>
2024-03-03 08:12:22 +0100

    Removed duplicates in item combat action

--- src/model/combat/ItemCombatAction.java
+++ src/model/combat/ItemCombatAction.java
@@ -11,6 +11,7 @@ import model.items.spells.CombatSpell;
 import model.states.CombatEvent;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -37,16 +38,18 @@ public class ItemCombatAction extends BasicCombatAction {
     @Override
     public List<CombatAction> getInnerActions(Model model) {
         List<CombatAction> res = new ArrayList<>();
+        Set<String> added = new HashSet<>();
         for (UsableItem item : usableItems) {
+            CombatAction action = null;
             if (item instanceof Scroll) {
                 if (((Scroll) item).getSpell() instanceof CombatSpell) {
                     CombatSpell spell = ((CombatSpell) ((Scroll) item).getSpell());
                     if (spell.canBeCastOn(model, target)) {
-                        res.add(new ScrollCombatAction((Scroll)item));
+                        action = new ScrollCombatAction((Scroll)item);
                     }
                 }
             } else if (target instanceof GameCharacter && item.canBeUsedOn(model, (GameCharacter) target)) {
-                res.add(new BasicCombatAction(item.getName(), false, false) {
+                action = new BasicCombatAction(item.getName(), false, false) {
                     @Override
                     protected void doAction(Model model, CombatEvent combat, GameCharacter performer, Combatant target) {
                         GameCharacter character = (GameCharacter)target;
@@ -55,17 +58,23 @@ public class ItemCombatAction extends BasicCombatAction {
                         model.getParty().getInventory().remove(item);
                         character.addToAttitude(performer, 2);
                     }
-                });
+                };
             } else if (target instanceof Enemy && item instanceof ThrowablePotion) {
-                res.add(new BasicCombatAction(item.getName(), true, false) {
+                action = new BasicCombatAction(item.getName(), true, false) {
                     @Override
                     protected void doAction(Model model, CombatEvent combat, GameCharacter performer, Combatant target) {
                         ThrowablePotion tp = (ThrowablePotion)item;
                         tp.throwYourself(model, combat, performer, target);
                         model.getParty().getInventory().remove(item);
                     }
-                });
+                };
             }
+
+            if (action != null && !added.contains(action.getName())) {
+                res.add(action);
+                added.add(action.getName());
+            }
+
         }
         return res;
     }
