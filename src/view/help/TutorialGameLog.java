042d6d738e0905f5c7008f3ab02e1412a7e86cf2
erik <eanilsson88@gmail.com>
2024-03-05 08:33:09 +0100

    Export log to file

--- src/view/help/TutorialGameLog.java
+++ src/view/help/TutorialGameLog.java
@@ -7,8 +7,8 @@ public class TutorialGameLog extends HelpDialog {
             "By pressing the F2 button you can make the text area larger. Pressing F2 again will " +
             "enter full screen log mode.\n\n" +
             "In full screen log mode, you can scroll the log up and down with the up and down arrow. Or " +
-            "scroll a whole page by pressing PgUp or PgDwn.\n\n" +
-            // TODO: Support dumping the log to file?
+            "scroll a whole page by pressing PgUp or PgDwn. You can export the contents of the log to a file " +
+            "by pressing Control-P.\n\n" +
             "The printing speed of the text can be adjusted in the Settings menu. The 'Combat Speed' " +
             "determines the printing speed in combat, whereas 'Log Speed' is the printing speed outside " +
             "of combat.";
