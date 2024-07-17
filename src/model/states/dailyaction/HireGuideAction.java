e579421dbb22234ee66d9e52302e730a5808920f
erik <eanilsson88@gmail.com>
2024-07-17 21:01:29 +0200

    Fixed hiring for negative gold

--- src/model/states/dailyaction/HireGuideAction.java
+++ src/model/states/dailyaction/HireGuideAction.java
@@ -21,7 +21,7 @@ public class HireGuideAction extends GameState {
     public GameState run(Model model) {
         guideSay(this, "Hey, you! Are you heading into the wild? You'll need somebody who knows the country.");
         print("Hire the guide for " + DAYS + " days, cost of " + COST + " gold? (Y/N) ");
-        if (yesNoInput()) {
+        if (model.getParty().getGold() >= COST && yesNoInput()) {
             leaderSay("You've got yourself a deal.");
             guideSay(this, "You won't regret this.");
             println("You paid " + COST + " gold to the guide.");
@@ -48,11 +48,22 @@ public class HireGuideAction extends GameState {
         guideSay(state, "My contract is up tomorrow. " +
                 "I'm heading back to " + townOrCityClosest.getPlaceName() + " unless you want to extend our deal.");
         state.print("Pay the guide another " + COST + " gold to extend the contract " + DAYS + " days? (Y/N) ");
-        if (state.yesNoInput()) {
+        if (0 < model.getParty().getGold() && model.getParty().getGold() < COST) {
+            state.leaderSay("Unfortunately, this party is a little short on gold. " +
+                    "How about " + model.getParty().getGold() + " gold?");
+            guideSay(state, "Fine... I'll stick around for a few more days. " +
+                    "It's not like I really have any other prospects out here.");
+            state.println("You paid " + model.getParty().getGold() + " gold to the guide.");
+            int days = model.getParty().getGold() + 1;
+            model.getParty().addToGold(-model.getParty().getGold());
+            model.getParty().addToGuide(days);
+            state.println("The guide will stay with you for an additional " + days + " days.");
+        } else if (model.getParty().getGold() >= COST && state.yesNoInput()) {
             state.println("You paid " + COST + " gold to the guide.");
             model.getParty().addToGold(-COST);
             model.getParty().addToGuide(DAYS);
             guideSay(state, "Glad to be of service.");
+            state.println("The guide will stay with you for an additional " + DAYS + " days.");
         } else {
             state.leaderSay("No thanks.");
             guideSay(state, "Then this is where are paths diverge. So long.");
