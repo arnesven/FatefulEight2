d9912f03c09d72929c6d1714a0990f2dbc7d5581
erik <eanilsson88@gmail.com>
2024-07-25 20:25:45 +0200

    Some more dog tutorial

--- src/view/help/DogsTutorial.java
+++ src/view/help/DogsTutorial.java
@@ -3,7 +3,11 @@ package view.help;
 import view.GameView;
 
 public class DogsTutorial extends HelpDialog {
-    private static final String TEXT = "Dogs can assist you in your journeys. Dogs are good at finding things.";
+    private static final String TEXT =
+            "Dogs can assist you in your journeys. Dogs are good at finding things and may lead " +
+            "you to more favorable events.\n\n" +
+            "Dogs can accompany you almost anywhere and never cost any rations to keep.\n" +
+            "Your dog will show up in the Inventory view under Mounts";
 
     public DogsTutorial(GameView view) {
         super(view, "Dogs", TEXT);
