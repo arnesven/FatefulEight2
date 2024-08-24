502c2f1143615a89b5a92a22129639109c63a703
erik <eanilsson88@gmail.com>
2024-08-24 14:25:52 +0200

    potion tips from magic expert

--- src/model/states/events/MagicExpertGeneralInteractionEvent.java
+++ src/model/states/events/MagicExpertGeneralInteractionEvent.java
@@ -25,6 +25,20 @@ public abstract class MagicExpertGeneralInteractionEvent extends GeneralInteract
             "The longer a vampire lives, the strong it becomes. A fully grown vampire is very fast, strong and intelligent. Be wary of such dark fiends.",
             "Vampires can turn into bats. Probably pretty convenient for them."
     );
+    private static final List<String> POTION_TIPS = List.of(
+            "Health potions can come in different sizes.",
+            "Stamina potions can give a person what little extra umph they need to accomplish a task.",
+            "Rejuvenation potions are pretty useful. They're a combination of health and stamina potions, " +
+                    "although less powerful than either of them.",
+            "Reviving Elixirs can bring somebody back to life, but they're pretty expensive to make.",
+            "Use a strength potion if you have some physical activities that you need to do and cannot risk failing at them.",
+            "Use a dexterity potion if you are going to do activities that require mobility or finesse.",
+            "Use a wits potion for days when you intend to use your brain and senses a lot.",
+            "Use a charisma potion if you need to boost your social skills.",
+            "If you really want to go somewhere and stay out of sight, try an invisibility potion.",
+            "Never drink a sleeping potion. But you could try to throw one at an enemy.",
+            "Unstable potions can explode. Be careful with them!"
+    );
 
     public MagicExpertGeneralInteractionEvent(Model model, String interactText, int stealMoney) {
         super(model, interactText, stealMoney);
@@ -33,6 +47,7 @@ public abstract class MagicExpertGeneralInteractionEvent extends GeneralInteract
     @Override
     protected Map<String, MyPair<String, String>> makeSpecificTopics(Model model) {
         Map<String, MyPair<String, String>> map = new HashMap<>();
+        map.put("potions", new MyPair<>("Know any good potions?", MyRandom.sample(POTION_TIPS)));
         map.put("spells", new MyPair<>("Got any good tips about spells?",
                 MyRandom.sample(SPELL_TIPS)));
         map.put("vampires", new MyPair<>("Know anything about vampires?",
