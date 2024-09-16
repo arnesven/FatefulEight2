b4bbffe6c245722a2d4760323714dab995cffd33
erik <eanilsson88@gmail.com>
2024-09-16 10:20:41 +0200

    Fixed missing copy implementation

--- src/model/items/weapons/TwinNunchucks.java
+++ src/model/items/weapons/TwinNunchucks.java
@@ -1,7 +1,14 @@
 package model.items.weapons;
 
+import model.items.Item;
+
 public class TwinNunchucks extends WeaponPair {
     public TwinNunchucks() {
         super(new Nunchuck(), new Nunchuck());
+    }
+
+    @Override
+    public Item copy() {
+        return new TwinNunchucks();
     }
 }
--- src/model/items/weapons/TwinSais.java
+++ src/model/items/weapons/TwinSais.java
@@ -1,7 +1,14 @@
 package model.items.weapons;
 
+import model.items.Item;
+
 public class TwinSais extends WeaponPair {
     public TwinSais() {
         super(new Sai(), new Sai());
+    }
+
+    @Override
+    public Item copy() {
+        return new TwinSais();
     }
 }
