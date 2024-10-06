894d571c67b10a9e23e065ff5d200b293a4ba866
erik <eanilsson88@gmail.com>
2024-10-06 07:22:34 +0200

    refactor and bugfix

--- src/model/states/dailyaction/TownDailyActionState.java
+++ src/model/states/dailyaction/TownDailyActionState.java
@@ -24,31 +24,26 @@ public class TownDailyActionState extends AdvancedDailyActionState {
         super.addNode(3, 4, new StayHereNode());
         super.addNode(urbanLocation.getTavernPosition().x, urbanLocation.getTavernPosition().y, new TavernNode(freeLodging));
         super.addNode(3, 3, new TownHallNode());
+        addTentOrHq(model, freeRations);
+        super.addNode(2, TOWN_MATRIX_ROWS-1, new WorkBenchNode(model, TownSubView.GROUND_COLOR, TownSubView.GROUND_COLOR_NIGHT));
+        super.addNode(4, TOWN_MATRIX_ROWS-1, new StableNode(model, TownSubView.GROUND_COLOR, TownSubView.GROUND_COLOR_NIGHT));
+        addNode(7, 2, new SaveGameNode());
+        addNode(7, 1, new FlagPoleNode());
+        addTravelNodes(model, isCoastal);
+        addShopsAndMore(model);
+        model.getMainStory().handleTownSetup(this);
+    }
+
+    private void addTentOrHq(Model model, boolean freeRations) {
         if (model.getParty().hasHeadquartersIn(urbanLocation)) {
             super.addNode(0, TOWN_MATRIX_ROWS - 1, new HeadquartersNode(model));
         } else {
             super.addNode(0, TOWN_MATRIX_ROWS - 1, new CampOutsideOfTownNode(freeRations, model,
                     TownSubView.GROUND_COLOR, TownSubView.GROUND_COLOR_NIGHT, "Make camp on the outskirts of town"));
         }
-        super.addNode(2, TOWN_MATRIX_ROWS-1, new WorkBenchNode(model, TownSubView.GROUND_COLOR, TownSubView.GROUND_COLOR_NIGHT));
-        super.addNode(4, TOWN_MATRIX_ROWS-1, new StableNode(model, TownSubView.GROUND_COLOR, TownSubView.GROUND_COLOR_NIGHT));
-        super.addNode(urbanLocation.getTravelNodePosition().x, urbanLocation.getTravelNodePosition().y,
-                new TravelNode(model, TownSubView.GROUND_COLOR, TownSubView.GROUND_COLOR_NIGHT));
-        addNode(7, 2, new SaveGameNode());
-        addNode(7, 1, new FlagPoleNode());
-        if (isCoastal && !urbanLocation.noBoat()) {
-            if (model.getDay() % urbanLocation.charterBoatEveryNDays() == 1) {
-                addNode(1, 0, new GoTheDocksNode(model));
-                addNode(4, 0, new CharterBoatAtDocks(model));
-            } else {
-                addNode(2, 0, new GoTheDocksNode(model));
-            }
-            if (urbanLocation.bothBoatAndCarriage()) {
-                addNode(TOWN_MATRIX_COLUMNS-1, 5, new GoToCarriageNode(model));
-            }
-        } else {
-            addNode(4, 0, new GoToCarriageNode(model));
-        }
+    }
+
+    private void addShopsAndMore(Model model) {
         for (GeneralShopNode shop : urbanLocation.getShops(model)) {
             addNode(shop.getColumn(), shop.getRow(), shop);
         }
@@ -56,7 +51,26 @@ public class TownDailyActionState extends AdvancedDailyActionState {
         if (careerOfficePosition != null) {
             addNode(careerOfficePosition.x, careerOfficePosition.y, new CareerOfficeNode());
         }
-        model.getMainStory().handleTownSetup(this);
+    }
+
+    private void addTravelNodes(Model model, boolean isCoastal) {
+        super.addNode(urbanLocation.getTravelNodePosition().x, urbanLocation.getTravelNodePosition().y,
+                new TravelNode(model, TownSubView.GROUND_COLOR, TownSubView.GROUND_COLOR_NIGHT));
+        if (isCoastal) {
+            if (!urbanLocation.noBoat()) {
+                if (model.getDay() % urbanLocation.charterBoatEveryNDays() == 1) {
+                    addNode(1, 0, new GoTheDocksNode(model));
+                    addNode(4, 0, new CharterBoatAtDocks(model));
+                } else {
+                    addNode(2, 0, new GoTheDocksNode(model));
+                }
+            }
+            if (urbanLocation.bothBoatAndCarriage()) {
+                addNode(TOWN_MATRIX_COLUMNS-1, 5, new GoToCarriageNode(model));
+            }
+        } else {
+            addNode(4, 0, new GoToCarriageNode(model));
+        }
     }
 
     public TownDailyActionState(Model model, boolean isCoastal, UrbanLocation urbanLocation) {
