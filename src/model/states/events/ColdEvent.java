9962b06ab16d93a6c7e33b4a7cc2155b52f1fc1c
erik <eanilsson88@gmail.com>
2025-03-02 08:18:07 +0100

    Endurance check in cold event

--- src/model/states/events/ColdEvent.java
+++ src/model/states/events/ColdEvent.java
@@ -1,7 +1,10 @@
 package model.states.events;
 
 import model.Model;
+import model.characters.GameCharacter;
+import model.classes.Skill;
 import model.states.DailyEventState;
+import util.MyRandom;
 
 import java.util.List;
 
@@ -12,12 +15,20 @@ public class ColdEvent extends DailyEventState {
 
     @Override
     protected void doEvent(Model model) {
-        model.getParty().randomPartyMemberSay(model,
+        println("The cold environment is affecting the party.");
+
+        List<GameCharacter> gcs =
+                model.getParty().doCollectiveSkillCheckWithFailers(model, this, Skill.Endurance, 8);
+        if (gcs.isEmpty()) {
+            return;
+        }
+        partyMemberSay(MyRandom.sample(gcs), MyRandom.sample(
                 List.of("Brrr... I'm so cold!", "My pants, they're like ice.",
                         "I'm shaking so much that I feel like I'm dancing.",
-                        "Can we please go someplace warmer?"));
-        println("The frigid cold has the party members shivering to their " +
+                        "Can we please go someplace warmer?")));
+
+        println("The frigid cold has the some of the party members shivering to their " +
                 "teeth. They must consume more food to stay warm.");
-        model.getParty().addToFood(-model.getParty().size());
+        model.getParty().addToFood(-gcs.size());
     }
 }
