aef4096a19fe5157e0139411373d567c70c61349
erik <eanilsson88@gmail.com>
2025-03-02 10:54:46 +0100

    Entertainment in company event

--- src/model/map/WorldHex.java
+++ src/model/map/WorldHex.java
@@ -269,6 +269,7 @@ public abstract class WorldHex {
     protected abstract SubView getSubView();
 
     private DailyEventState generateOnRoadEvent(Model model) {
+        return new CompanyEvent(model); /*
         int dieRoll = MyRandom.rollD10();
         if (5 <= dieRoll && dieRoll <= 8) {
             List<DailyEventState> events = new ArrayList<>();
@@ -299,7 +300,7 @@ public abstract class WorldHex {
         } else if (dieRoll >= 9) {
             return generateTerrainSpecificEvent(model);
         }
-        return new NoEventState(model);
+        return new NoEventState(model); */
     }
 
     public void travelTo(Model model) {
--- src/model/states/events/CompanyEvent.java
+++ src/model/states/events/CompanyEvent.java
@@ -4,10 +4,12 @@ import model.Model;
 import model.characters.GameCharacter;
 import model.characters.PersonalityTrait;
 import model.classes.Classes;
+import model.classes.Skill;
 import model.enemies.BanditEnemy;
 import model.enemies.Enemy;
 import model.enemies.SoldierEnemy;
 import model.enemies.SoldierLeaderEnemy;
+import model.items.accessories.ShieldItem;
 import model.states.CombatEvent;
 import model.states.DailyEventState;
 import util.MyLists;
@@ -58,13 +60,17 @@ public class CompanyEvent extends DailyEventState {
         }
         int soldiers = model.getParty().partyStrength() / 12 + 2;
         int loss = Math.min(soldiers + 1, model.getParty().getFood());
-        print("Do you accept (Y) losing " + loss + " rations and indulging in their tiresome games or risk (N) angering the company of soldiers? ");
-        if (yesNoInput()) {
-            model.getParty().addToFood(-loss);
-            MyLists.forEach(model.getParty().getPartyMembers(), (GameCharacter gc) -> gc.addToSP(-1));
-            println("You grudgingly hand over your packs of food.");
-            println("Each party member exhausts 1 SP.");
-        } else {
+
+        println("Do you accept losing " + loss +
+                " rations and indulge in their tiresome games, or do you handle the situation in another way?");
+        int selectedChoice = multipleOptionArrowMenu(model, 24, 24, List.of("Give in to soldiers",
+                "Distract with entertainment", "Beat them up", "Run away"));
+
+        if (selectedChoice == 0) {
+            foodAndSpLoss(model, loss);
+        } else if (selectedChoice == 1) {
+            tryEntertainment(model, loss);
+        } else if (selectedChoice == 2) {
             List<Enemy> enemies = new ArrayList<>();
             for (int i = 0; i < soldiers - 2; i++){
                 enemies.add(new SoldierEnemy('A'));
@@ -73,6 +79,61 @@ public class CompanyEvent extends DailyEventState {
             enemies.add(new SoldierLeaderEnemy('B'));
             runCombat(enemies);
             possiblyGetHorsesAfterCombat("soldiers", soldiers);
+        } else {
+            println("You quickly scurry away from the company of soldiers.");
+            setFledCombat(true);
         }
+    }
+
+    private void tryEntertainment(Model model, int loss) {
+        if (model.getParty().doCollaborativeSkillCheck(model, this, Skill.Entertain, 8)) {
+            println("You quickly improvise a little play and musical number, full of slap-stick humor and " +
+                    "inappropriate innuendo. The company of soldiers appreciate it greatly and forget about " +
+                    "their own stupid games.");
+            if (model.getParty().size() > 1) {
+                leaderSay("Good work team!");
+            }
+        } else {
+            println("The company of soldiers is outraged by your poor performance and start throwing bottles and " +
+                    "other trash at you.");
+            portraitSay("Amateurs! What a shoddy performance!");
+            for (GameCharacter gc : model.getParty().getPartyMembers()) {
+                if (MyRandom.randInt(3) > 0) {
+                    String thing = MyRandom.sample(List.of("shoe", "rotten apple", "rock",
+                            "bottle", "leather strap", "stick"));
+                    print("Somebody throws a " + thing + " at " + gc.getName() + "! ");
+                    if (MyRandom.rollD10() <= gc.getSpeed()) {
+                        println("But " + heOrShe(gc.getGender()) + " quickly " +
+                                MyRandom.sample(List.of("dodges it.", "evades it.",
+                                        "moves out of the way.", "ducks.")));
+                    } else if (gc.getEquipment().getAccessory() instanceof ShieldItem &&
+                            MyRandom.rollD10() <= ((ShieldItem)gc.getEquipment().getAccessory()).getBlockChance()) {
+                        println("Clonk! The " + thing + " bounces off of " + gc.getFirstName() + "'s shield.");
+                    } else {
+                        println("It strikes " + himOrHer(gc.getGender()) + " right in the face.");
+                        println(gc.getFirstName() + " takes 1 damage.");
+                        gc.addToHP(-1);
+                        if (gc.isDead()) {
+                            characterDies(model, this, gc,
+                                    " has been killed by the flying " + thing + "!", true);
+                        } else {
+                            partyMemberSay(gc, MyRandom.sample(List.of("Ow! That hurt", "Hey!", "What the heck?",
+                                    "Ouch! Cut it out!")));
+                        }
+                    }
+                }
+            }
+            if (!model.getParty().isWipedOut()) {
+                leaderSay("I guess we'd better think of something else.");
+                foodAndSpLoss(model, loss);
+            }
+        }
+    }
+
+    private void foodAndSpLoss(Model model, int loss) {
+        model.getParty().addToFood(-loss);
+        MyLists.forEach(model.getParty().getPartyMembers(), (GameCharacter gc) -> gc.addToSP(-1));
+        println("You grudgingly hand over your packs of food and take part in the shenanigans.");
+        println("Each party member exhausts 1 SP.");
     }
 }
