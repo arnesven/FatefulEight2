a83f4117946496d8fe7dbba8bd15d02b5fa90d72
erik <eanilsson88@gmail.com>
2025-05-17 23:33:12 +0200

    Nicer werebear enemy and sprite

--- src/model/enemies/WerebearEnemy.java
+++ src/model/enemies/WerebearEnemy.java
@@ -1,7 +1,53 @@
 package model.enemies;
 
-public class WerebearEnemy extends WerewolfEnemy { // TODO: make cooler
+import model.Model;
+import model.combat.loot.CombatLoot;
+import model.combat.loot.MonsterCombatLoot;
+import model.enemies.behaviors.KnockDownAttackBehavior;
+import view.MyColors;
+import view.sprites.LoopingSprite;
+import view.sprites.Sprite;
+
+public class WerebearEnemy extends BeastEnemy {
+    private static final Sprite SPRITE = new WerebearSprite();
+
     public WerebearEnemy(char a) {
-        super(a);
+        super(a, "Werebear", RAMPAGING, new KnockDownAttackBehavior(3));
+    }
+
+    @Override
+    protected Sprite getSprite() {
+        return SPRITE;
+    }
+
+    @Override
+    public int getDamage() {
+        return 4;
+    }
+
+    @Override
+    public CombatLoot getLoot(Model model) {
+        return new MonsterCombatLoot(model);
+    }
+
+    @Override
+    public int getMaxHP() {
+        return 16;
+    }
+
+    @Override
+    public int getSpeed() {
+        return 2;
+    }
+
+    private static class WerebearSprite extends LoopingSprite {
+        public WerebearSprite() {
+            super("werebear", "enemies.png", 0x130, 32);
+            setFrames(4);
+            setColor1(MyColors.BLACK);
+            setColor2(MyColors.BROWN);
+            setColor3(MyColors.PINK);
+            setColor4(MyColors.TAN);
+        }
     }
 }
