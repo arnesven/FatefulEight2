1d7c852230b6086a31b7abc72c7c3d9e9611ba4a
erik <eanilsson88@gmail.com>
2025-12-11 11:47:04 +0100

    Fields labor event gives smaller reward.

--- src/model/states/events/FieldsLaborEvent.java
+++ src/model/states/events/FieldsLaborEvent.java
@@ -78,7 +78,7 @@ public abstract class FieldsLaborEvent extends FarmerEvent {
         if (!didSay) {
             randomSayIfPersonality(PersonalityTrait.greedy, new ArrayList<>(), "A measly few coins for all that work?");
         }
-        model.getParty().earnGold(20);
+        model.getParty().earnGold(MyRandom.randInt(5, 20));
         new GuestEvent(model, getPortrait()).doEvent(model);
         freeRations = true;
         didSay = randomSayIfPersonality(PersonalityTrait.benevolent, new ArrayList<>(),
